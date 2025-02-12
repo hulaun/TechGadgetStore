ef7e32727296b66a621dea14909910c6d83d3681
Mai Van Thi <poiseidoncoder@gmail.com>
2025-02-13 00:22:26 +0700

    #1 init cart screen

---
+++ frontend/app/(cart)/index.tsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CartScreen() {
+  const cartSampleDate = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'Product 1 description',
+      price: 100,
+      qty: 1
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'Product 2 description',
+      price: 200,
+      qty: 1
+    }
+  ]
+
+  return (
+    <View>
+
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
